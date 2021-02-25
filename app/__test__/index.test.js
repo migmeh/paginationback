f9a054f853cca243b20cd7ecd916b061c529d4fb
migmeh <xmigmeh@gmail.com>
2021-02-25 16:28:46 -0600

    my testing

--- app/__test__/index.test.js
+++ app/__test__/index.test.js
@@ -5,7 +5,7 @@ const request = supertest(app)
 
 let testServer
 beforeAll(()=>{
-    testServer = app.listen(4000);
+    testServer = app.listen(8081);
 });
 
 
@@ -23,11 +23,32 @@ describe('test for jest', ()=> {
 
 describe('test for endpoint', ()=>{
 
+    it('get', async done => {
+        const response = await request.get('/')
+
+        expect(response.error).toBe(false);
+        expect(response.status).toBe(200);
+        expect(response.body.body).not.toBeNull();
+        done()
+    })
+
+    it('gets the test endpoint', async done => {
+        const response = await request.get('/api/tutorials?page=1')
+
+        expect(response.error).toBe(false);
+        expect(response.status).toBe(200);
+        expect(response.body.body).not.toBeNull();
+        done()
+    })
+
+
+
     it('gets the test endpoint', async done => {
         const response = await request.get('/api/tutorials?page=1&size=5')
 
         expect(response.error).toBe(false);
         expect(response.status).toBe(200);
+        expect(response.body.body).not.toBeNull();
         done()
     })
 
